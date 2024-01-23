0f8a4f91240458fee3442e0947fabe6e9efc2613
saif <saif-aknouch@hotmail.com>
2024-01-23 12:58:01 +0100

    Initialise A and J matrix

--- A_new_datastru/Markov.py
+++ A_new_datastru/Markov.py
@@ -9,3 +9,11 @@ def define_sequence(seq_str):
     amino_acid_map = {'H': 1, 'P': 2, 'C': 3}
     return [amino_acid_map[acid] for acid in seq_str]
 
+def initialize(N_aminoacids, seq):
+    A = np.array(define_sequence(seq))
+    J = np.zeros((4, 4))
+    J[1, 1] = -1  # HH interactie
+    J[3, 3] = -5  # CC interactie
+    return A, J
+
+
