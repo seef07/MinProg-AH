4d028d878f7981376496f7b5507a90c3871829e1
saif <saif-aknouch@hotmail.com>
2024-01-23 13:04:25 +0100

    SAW data

--- A_new_datastru/park.py
+++ A_new_datastru/park.py
@@ -43,3 +43,26 @@ def phase_data(N_aminoacids=15):
             file.write("{} {} {} {} {} {} {}\n".format(
                 t, mean_E, se_E, std_E, mean_L, se_L, std_L))
 
+def SAW_data(N_aminoacids=15):
+    temp_int = 10
+    temp_final = 0.5
+    dtemp = 0.5
+    temp_step = int((temp_int - temp_final) / dtemp) + 1
+    temp_array = np.linspace(temp_int, temp_final, temp_step, endpoint=True)
+    
+    fname = 'SAW_UniJ1_{}.txt'.format(N_aminoacids)
+    a = np.loadtxt(fname)
+    E = a[:, 1]
+    L = a[:, 2]
+    mean_E = np.mean(E)
+    mean_L = np.mean(L)
+    std_E = np.std(E)
+    std_L = np.std(L)
+    se_E = std_E / np.sqrt(len(E))
+    se_L = std_L / np.sqrt(len(L))
+
+    with open('SAW_UniJ1_phase_{}.txt'.format(N_aminoacids), 'w') as file:
+        for t in temp_array:
+            file.write("{} {} {} {} {} {} {}\n".format(
+                t, mean_E, se_E, std_E, mean_L, se_L, std_L))
+            
