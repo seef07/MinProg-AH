02f1623e0232e93969538e41f6d47eb4df967c65
saif <saif-aknouch@hotmail.com>
2024-01-23 13:03:27 +0100

    park functions for later use

---
+++ A_new_datastru/park.py
@@ -0,0 +1,20 @@
+def phase_transition(N_aminoacids, seq):
+    A, J = initialize(N_aminoacids, seq)
+
+    N_steps = 2000000
+    ave_int = 1800000
+    temp_int = 30
+    temp_final = 20
+    dtemp = 0.5
+    temp_step = int((temp_int - temp_final) / dtemp) + 1
+    temp_array = np.linspace(temp_int, temp_final, temp_step, endpoint=True)
+
+    for t in temp_array:
+        fname = 'UniJ5_equilibrium_{}_T{:.1f}.txt'.format(N_aminoacids, t)
+        with open(fname, 'w') as file:
+            for n in range(10):
+                step, FreeEnergy, L, R = ProteinFolding_jit(N_aminoacids, J, A, t, N_steps)
+                for i in range(len(step)):
+                    if step[i] < ave_int:
+                        continue
+                    file.write('{} {} {}\n'.format(step[i], FreeEnergy[i], L[i]))
