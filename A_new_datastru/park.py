cf78d042e7846314d2fbbb480bb6a228e7cba59d
saif <saif-aknouch@hotmail.com>
2024-01-23 13:04:06 +0100

    Phase data

--- A_new_datastru/park.py
+++ A_new_datastru/park.py
@@ -29,3 +29,17 @@ def equilibrium_mean_SD(fname):
     se_L = std_L / np.sqrt(len(L))
     return np.mean(E), np.mean(L), std_E, se_E, std_L, se_L
 
+def phase_data(N_aminoacids=15):
+    temp_int = 30
+    temp_final = 0.5
+    dtemp = 0.5
+    temp_step = int((temp_int - temp_final) / dtemp) + 1
+    temp_array = np.linspace(temp_int, temp_final, temp_step, endpoint=True)
+    
+    with open('UniJ5_phase_{}.txt'.format(N_aminoacids), 'w') as file:
+        for t in temp_array:
+            fname = "UniJ5_equilibrium_{}_T{:.1f}.txt".format(N_aminoacids, t)
+            mean_E, mean_L, std_E, se_E, std_L, se_L = equilibrium_mean_SD(fname)
+            file.write("{} {} {} {} {} {} {}\n".format(
+                t, mean_E, se_E, std_E, mean_L, se_L, std_L))
+
