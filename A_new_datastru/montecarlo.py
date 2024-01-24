3db99f7a105cb493665b2ad36250d9363bf2a464
saif <saif-aknouch@hotmail.com>
2024-01-24 19:58:08 +0100

    New reinforcement algorithm

--- A_new_datastru/montecarlo.py
+++ A_new_datastru/montecarlo.py
@@ -5,11 +5,11 @@ import matplotlib.colors as mcolors
 
 
 # Protein sequence
-sequence = "HHPHHHPHPHHHPH"  # Change this to your protein sequence
+sequence = "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH"  # Change this to your protein sequence
 
 # Energy matrix
 energy_matrix = {
-    'HH': -1, 'CC': -5, 'CH': 0, 'HC': 0, 
+    'HH': -1, 'CC': -5, 'CH': -1, 'HC':-1, 
     'HP': 0, 'PH': 0, 'PP': 0, 'PC': 0, 'CP': 0
 }
 
